5602b36703ec8b7fb35fd71fbc97f8f3988e8d97
maligonD <maligond42gmai.com>
2023-05-12 14:27:51 +0300

    Rename vars

--- app/src/main/java/com/example/appfragmentsapple/FragmentAppleStorage.kt
+++ app/src/main/java/com/example/appfragmentsapple/FragmentAppleStorage.kt
@@ -28,18 +28,18 @@ class FragmentAppleStorage : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         dataModel.apply {
-            countApples.observe(activity as LifecycleOwner) {
+            countApple.observe(activity as LifecycleOwner) {
                 countApplesGet = it
             }
         }.also { it ->
-            it.greenApples.observe(activity as LifecycleOwner){
+            it.greenApple.observe(activity as LifecycleOwner){
                 if (it == true){
                     countGreenApples += countApplesGet
                     binding.textCountGreen.text = countGreenApples.toString()
                 }
             }
         }.also { it ->
-            it.redApples.observe(activity as LifecycleOwner){
+            it.redApple.observe(activity as LifecycleOwner){
                 if (it == true){
                     countRedApples += countApplesGet
                     binding.textCountRed.text = countRedApples.toString()
